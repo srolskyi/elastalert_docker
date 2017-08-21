168d6e40218005f67411fb3e7b5688d553826ad1
Sergii Rolskyi <serg@arch-e450>
2017-08-21 16:26:23 +0300

    add --username and --pasword to start.sh

--- start.sh
+++ start.sh
@@ -48,7 +48,7 @@ sleep 5
 if ! wget -q -T 3 -O - "${WGET_SCHEMA}${WGET_AUTH}${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/${ELASTALERT_INDEX}" 2>/dev/null
 then
     echo "Creating Elastalert index in Elasticsearch..."
-    elastalert-create-index ${CREATE_EA_OPTIONS} --host "${ELASTICSEARCH_HOST}" --port "${ELASTICSEARCH_PORT}" --config "${ELASTALERT_CONFIG}" --index ${ELASTALERT_INDEX} --old-index ""
+    elastalert-create-index ${CREATE_EA_OPTIONS} --host "${ELASTICSEARCH_HOST}" --port "${ELASTICSEARCH_PORT}" --username "${ELASTICSEARCH_USER}" --password "${ELASTICSEARCH_PASSWORD}" --config "${ELASTALERT_CONFIG}" --index ${ELASTALERT_INDEX} --old-index ""
 else
     echo "Elastalert index already exists in Elasticsearch."
 fi
