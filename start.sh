c5f5b0572f7023dc90a6dd1aac1720abe8e8951d
Serg Rolskyi <sergii.rolskyi@linux-tricks.net>
2017-08-22 13:39:36 +0300

    up start.sh

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ esac
 
 # Set the timezone.
 if [ "$SET_CONTAINER_TIMEZONE" = "true" ]; then
-        timedatectl set-timezone "${CONTAINER_TIMEZONE}" && \
+        timedatectl set-timezone "$CONTAINER_TIMEZONE" && \
 	echo "Container timezone set to: $CONTAINER_TIMEZONE"
 else
 	echo "Container timezone not modified"
